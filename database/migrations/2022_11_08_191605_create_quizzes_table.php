bc2de81c90c3f64051ea3b8ee5ae7bae51feb2c5
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-08 20:41:14 +0100

    Adding quiz model (with migration, factory and seeder)

---
+++ database/migrations/2022_11_08_191605_create_quizzes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('author_id')->references('id')->on('users')->nullable();
+            $table->foreignId('correct_answer_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
