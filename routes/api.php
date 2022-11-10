18c9cf9f9fc0e60b0fa750011acdf1b953b1982b
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-10 22:12:32 +0100

    Refactoring routes

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('quizzes', function() {})->name('quizzes-create');
-
-Route::put('quizzes/{id}', function() {})->name('quizzes-edit');
-
-Route::delete('/quizzes/{id}', function() {})->name('quizzes-delete');
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
-Route::get('/quizzes', [QuizzesController::class, 'index'])->name('quizzes-index');
-
-Route::get('/quizzes/{id}', [QuizzesController::class, 'show'])->name('quizzes-show');
+Route::resource('quizzes', QuizzesController::class);
