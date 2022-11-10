dc6b5f7d0a3fa01d10a5e010a562641a7a07be3c
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-10 22:41:37 +0100

    put author_id as soft link because log in is not implemented

--- database/migrations/2022_11_08_191605_create_quizzes_table.php
+++ database/migrations/2022_11_08_191605_create_quizzes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->foreignId('author_id')->references('id')->on('users')->nullable();
+            $table->foreignId('author_id')->nullable();
             $table->timestamps();
         });
     }
