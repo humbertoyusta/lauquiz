e59f84c5256a69ea25a85e3aedfbfb53b13bce2a
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-11 23:10:36 +0100

    Adding default value to form input

--- resources/views/components/form-input-text.blade.php
+++ resources/views/components/form-input-text.blade.php
@@ -1,8 +1,8 @@
-@props(['name', 'errors'])
+@props(['name', 'errors', 'value'])
 
 <div>
     <div class="form-floating mb-3">
-        <input type="text" class="form-control" id="{{$name}}" name="{{$name}}" placeholder="{{$name}}">
+        <input type="text" class="form-control" id="{{$name}}" name="{{$name}}" placeholder="{{$name}}" value="{{old($name, $value)}}">
         <label for="{{$name}}">{{ucfirst($name)}}</label>
     </div>
     <x-form-input-error :name="$name" :errors="$errors" />
--- resources/views/quizzes/create.blade.php
+++ resources/views/quizzes/create.blade.php
@@ -2,7 +2,7 @@
     <div class="card m-auto mt-5" style="width: 30rem;">
         <x-form route="{{route('quizzes.store')}}" method="POST">
             @csrf
-            <x-form-input-text name="title" :errors="$errors" />
+            <x-form-input-text name="title" :errors="$errors" value="" />
         </x-form>
     </div>
 </x-site-layout>
