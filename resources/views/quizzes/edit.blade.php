b6e66181a6e23c3376073199db489a8929ed3e46
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-13 18:34:55 +0100

    displaying questions in quizzes view

--- resources/views/quizzes/edit.blade.php
+++ resources/views/quizzes/edit.blade.php
@@ -4,5 +4,14 @@
             @csrf
             <x-form-input-text name="title" :errors="$errors" :value="$quiz['title']" />
         </x-form>
+        <ol class="list-group mt-5">
+            @foreach($quiz->questions as $question)
+                <x-bar :text="$question->content">
+                    <x-get-button :route="route('questions.edit', ['question' => $question['id']])" name="Edit" />
+                    <x-delete-button :route="route('questions.destroy', ['question' => $question['id']])" />
+                </x-bar>
+            @endforeach
+            <x-get-button :route="route('questions.create', ['quiz_id' => $quiz['id']])" name="Add new question" />
+        </ol>
     </div>
 </x-site-layout>
