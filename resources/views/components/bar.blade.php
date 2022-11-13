3deee69196fe753708a553d5c661da57d115c4cf
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-13 18:38:45 +0100

    small layout fix

--- resources/views/components/bar.blade.php
+++ resources/views/components/bar.blade.php
@@ -1,7 +1,7 @@
 @props(['text'])
 
 <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2">
+    <div class="ms-2 w-75">
         <div class="fw-bold">{{$text}}</div>
     </div>
     {{$slot}}
