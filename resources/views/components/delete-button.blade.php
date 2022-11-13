e76ef5a7260ccf4ba5c4d9e7a08f821a7166e916
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-13 23:13:12 +0100

    Putting components inside divs

--- resources/views/components/delete-button.blade.php
+++ resources/views/components/delete-button.blade.php
@@ -1,7 +1,9 @@
 @props(['route'])
 
-<form action="{{$route}}" method="POST">
-    @method('DELETE')
-    @csrf
-    <button type="submit" class="btn btn-outline-danger">Delete</button>
-</form>
+<div>
+    <form action="{{$route}}" method="POST">
+        @method('DELETE')
+        @csrf
+        <button type="submit" class="btn btn-outline-danger">Delete</button>
+    </form>
+</div>
