72dfb1520d4eceab0bfc842b8d3f0973a4079ecb
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-13 18:34:35 +0100

    Adding get questions with answers to service

--- app/Services/QuestionsService.php
+++ app/Services/QuestionsService.php
@@ -2,10 +2,17 @@
 
 namespace App\Services;
 
+use App\Models\Question;
+
 class QuestionsService extends AbstractService
 {
     public function __construct()
     {
         parent::__construct('\App\Models\Question');
+    }
+
+    public function getQuestionWithAnswers (int $id)
+    {
+        return Question::where('id', $id)->with('answers')->first();
     }
 }
