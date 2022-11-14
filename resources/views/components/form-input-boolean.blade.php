2b2208d0ae4143409d2bbe54fa17bf2b578819a5
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-14 22:20:34 +0100

    Fixing checkbox

--- resources/views/components/form-input-boolean.blade.php
+++ resources/views/components/form-input-boolean.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'text', 'value'])
 
 <div class="form-check">
-  <input class="form-check-input" type="checkbox" value="1" name={{$name}} id="{{$name}}" {{ old('is_featured') ? 'checked="checked"' : '' }}>
+  <input class="form-check-input" type="checkbox" value="1" name={{$name}} id="{{$name}}" {{ old($name, $value) ? 'checked' : '' }}>
   <label class="form-check-label" for="flexCheckDefault">
     {{$text}}
   </label>
