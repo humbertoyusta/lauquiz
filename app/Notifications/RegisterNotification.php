ad6aeb6dc4f4a72e88ae93b23da7256171c1475e
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-17 20:47:46 +0100

    Creating RegisterNotification

---
+++ app/Notifications/RegisterNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RegisterNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->from('hello@lauquiz.com', 'Lauquiz')
+                    ->subject('Thank you for using Lauquiz')
+                    ->greeting('Dear '.$notifiable->name.',')
+                    ->line('Thank you for registering in our application!')
+                    ->line('Do you want to start by playing a quiz?')
+                    ->action('Quiz list', url(route('play.index')))
+                    ->salutation('Greetings<br>Lauquiz Team.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
