847682fc0434e98dec60b4303ee3142b269ab313
humbertoyusta02 <humbertoyusta02@gmail.com>
2022-11-21 23:32:31 +0100

    Minor change, move add answer button to the top of answers

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -8,13 +8,15 @@
         </x-form-multipart>
         <img src="{{$question->image}}" alt="Question Image" width="300" height="300" class="rounded mx-auto d-block" />
         <ol class="list-group mt-5">
+            <x-simple-bar>
+                <x-get-button :route="route('answers.create', ['quiz' => $quiz_id, 'question' => $question->id])" name="Add new Answer" />
+            </x-simple-bar>
             @foreach($question->answers as $answer)
                 <x-bar :text="$answer->content" extra_text="">
                         <x-get-button :route="route('answers.edit', ['quiz' => $quiz_id, 'question' => $question->id, 'answer' => $answer->id])" name="Edit Answer" />
                         <x-delete-button :route="route('answers.destroy', ['quiz' => $quiz_id, 'question' => $question->id, 'answer' => $answer->id])" />
                 </x-bar>
             @endforeach
-            <x-get-button :route="route('answers.create', ['quiz' => $quiz_id, 'question' => $question->id])" name="Add new Answer" />
         </ol>
         <div class="mt-4">
             <x-get-button route="{{route('quizzes.edit', ['quiz' => $quiz_id])}}" name="Done" />
