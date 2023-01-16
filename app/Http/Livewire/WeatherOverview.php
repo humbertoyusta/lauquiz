57915ee6266cfe220b05937085d143f2828134b0
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-16 23:49:57 +0100

    feat/add-livewire-weather-overview-component

---
+++ app/Http/Livewire/WeatherOverview.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Services\WeatherForecaService;
+use Livewire\Component;
+use Stevebauman\Location\Facades\Location;
+
+class WeatherOverview extends Component
+{
+    public array $todayOverview = [];
+
+    public array $weeklyOverview = [];
+
+    public string $city;
+
+    public bool $isDaily = true;
+
+    public function mount ()
+    {
+        $this->city = Location::get()->cityName;
+    }
+
+    public function render()
+    {
+        $weatherForecaService = new WeatherForecaService();
+
+        if ($this->isDaily) {
+            if (!count($this->todayOverview))
+                $this->todayOverview = $weatherForecaService->getTodayOverview();
+        } else {
+            if (!count($this->weeklyOverview))
+                $this->weeklyOverview = $weatherForecaService->getWeeklyOverview();
+        }
+
+        return view('livewire.weather-overview');
+    }
+}
--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -1,17 +1,6 @@
-<x-site-layout>
+<x-site-layout :usesLivewire="true">
     <div class="card m-auto mt-5" style="width: 36rem;">
         <x-application-logo />
-        <div class="card-body">
-            @if($weatherOverview)
-                <h4>Weather overview:</h4>
-                <p class="card-text">
-                    Currently, on {{$weatherOverview['city']}}, the temperature is 
-                    {{$weatherOverview['temperature']}} °C, it feels like 
-                    {{$weatherOverview['feelsLikeTemp']}} °C, the maximum temperature today is
-                    {{$weatherOverview['maxTemp']}} °C, and the minimum is
-                    {{$weatherOverview['minTemp']}} °C
-                </p>
-            @endif
-        </div>
+        <livewire:weather-overview></livewire:weather-overview>
     </div>
 </x-site-layout>
