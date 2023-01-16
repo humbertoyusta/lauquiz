181cff09341b8a80ebe542e7d916858789a1c1fe
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-16 19:54:10 +0100

    refactor/remove-weather-info-from-welcome-page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,28 +8,8 @@ use Illuminate\Support\Facades\Cache;
 
 class WelcomeController extends Controller
 {
-    private WeatherForecaService $weatherForecaService;
-
-    public function __construct(WeatherForecaService $weatherForecaService)
-    {
-        $this->weatherForecaService = $weatherForecaService;
-    }
-
     public function __invoke()
     {
-        try {
-            $weatherOverview = Cache::remember(
-                'welcome.weatherOverview',
-                config('app.cache_long_ttl'),
-                fn() => $this->weatherForecaService->getWeatherOverview()
-            );
-        } catch (Exception $e) {
-            $weatherOverview = null;
-        }
-
-        return view(
-            'welcome', 
-            ['weatherOverview' => $weatherOverview],
-        );
+        return view('welcome');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,6 @@
         <div class="card-body">
             <p class="card-text">Making simple quizzes has never been so easy.</p>
             <x-get-button :route="route('play.index')" name="Get Started"></x-get-button>
-            @if($weatherOverview)
-                <p class="card-text">
-                    Currently, on {{$weatherOverview['city']}}, the temperature is 
-                    {{$weatherOverview['temperature']}} °C, it feels like 
-                    {{$weatherOverview['feelsLikeTemp']}} °C, the maximum temperature today is
-                    {{$weatherOverview['maxTemp']}} °C, and the minimum is
-                    {{$weatherOverview['minTemp']}} °C
-                </p>
-            @endif
         </div>
     </div>
 </x-site-layout>
