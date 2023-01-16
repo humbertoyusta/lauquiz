0e906edc8c18a58236b88393660e31a023119e2f
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-16 22:37:23 +0100

    feat/add-uses-livewire-parameter-to-site-layout

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -8,6 +8,10 @@
 
         <!-- Bootstrap css -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+        @if($usesLivewire)
+            @livewireStyles
+        @endif
     </head>
     <body class="antialiased">
         
@@ -17,6 +21,10 @@
 
         <!-- Bootstrap JS -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+        @if($usesLivewire)
+            @livewireScripts
+        @endif
     </body>
 </html>
 
