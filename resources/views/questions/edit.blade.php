b0e010da28d74484efc130ac3d9277bf87972b65
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-17 23:11:40 +0100

    fix/add-livewire-to-edit-question

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -1,4 +1,4 @@
-<x-site-layout>
+<x-site-layout :usesLivewire="true">
     <x-page-title title="Edit a Question" />
     <div class="card m-auto mt-5 mb-5" style="width: 50rem;">
         <x-form-multipart route="{{route('questions.update', ['quiz' => $quiz_id, 'question' => $question->id])}}" method="PUT" button_name="Edit Question">
@@ -12,10 +12,7 @@
                 <x-get-button :route="route('answers.create', ['quiz' => $quiz_id, 'question' => $question->id])" name="Add new Answer" />
             </x-simple-bar>
             @foreach($question->answers as $answer)
-                <x-bar :text="$answer->content" extra_text="">
-                        <x-get-button :route="route('answers.edit', ['quiz' => $quiz_id, 'question' => $question->id, 'answer' => $answer->id])" name="Edit Answer" />
-                        <x-delete-button :route="route('answers.destroy', ['quiz' => $quiz_id, 'question' => $question->id, 'answer' => $answer->id])" />
-                </x-bar>
+                <livewire:answer-component :answer="$answer" />
             @endforeach
         </ol>
         <div class="mt-4">
