fbba0d9ea74db038adbcf82f0b7f0ab810701536
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-19 01:13:08 +0100

    fix/fixing-typos-for-renamed-components

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -1,4 +1,4 @@
 <x-site-layout :usesLivewire="true">
     <x-page-title title="Edit a Question" />
-    <livewire:question-component :question="$question" :image="$image" />
+    <livewire:question-parent-component :question="$question" :image="$image" />
 </x-site-layout>
