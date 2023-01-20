d4940764a63ef6e62201b53261b867390c2bb194
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-20 22:25:24 +0100

    fix/fixing-image-upload-if-tried-to-submit-too-early

--- app/Http/Livewire/QuestionParentComponent.php
+++ app/Http/Livewire/QuestionParentComponent.php
@@ -31,6 +31,16 @@ class QuestionParentComponent extends Component
         $this->content = $question->content;
     }
 
+    public function updatedImage ()
+    {
+        if (!$this->question->canBeEditedBy())
+            abort(403);
+
+        $this->question->storeImage($this->image);
+
+        $this->question->refresh();
+    }
+
     public function update ()
     {
         if (!$this->question->canBeEditedBy())
