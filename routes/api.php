9f16379a66218e3fcd23c05adb0227a177d7cab3
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-21 21:31:45 +0100

    feat/adding-mollie-integration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BuyQuizController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('mollie')->group(function () {
+    Route::post('webhook', [BuyQuizController::class, 'webhook'])->name('mollie.webhook');
 });
