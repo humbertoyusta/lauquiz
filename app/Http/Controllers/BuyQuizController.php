57c53e4d9fad34abddaf795610776f61acf0563a
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-21 18:33:30 +0100

    fix/remove-stripe

--- app/Http/Controllers/BuyQuizController.php
+++ app/Http/Controllers/BuyQuizController.php
@@ -3,26 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quiz;
-use App\Models\User;
-use Illuminate\Http\Request;
 
 class BuyQuizController extends Controller
 {
     public function show(Quiz $quiz) {
         $user = auth()->user();
-
-        return $user->checkoutCharge(
-            config('app.quizzes.default_price'),
-            'Quiz '.$quiz->title,
-            1,
-            [
-                'success_url' => route('play.index'),
-                'cancel_url' => route('play.index'),
-                'metadata' => [
-                    'user_id' => $user->id,
-                    'quiz_id' => $quiz->id,
-                ],
-            ],
-        );
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,14 +9,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     use SoftDeletes;
-    use Billable;
 
     /**
      * The attributes that are mass assignable.
