4064628808ab88f92daba62f1297f63bf35a68e4
humbertoyusta02 <humbertoyusta02@gmail.com>
2023-01-23 22:20:13 +0100

    feat/adding-default-user-and-admin-for-testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public User $user;
+    public User $admin;
+
+    protected function setUp(): void
+    {
+        $this->user = User::factory()->create();
+        $this->admin = User::factory()->create(['is_admin' => 'true']);
+    }
 }
